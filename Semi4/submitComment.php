3f5ca0c8f51879417fb4c65905560f329cd3da31
William Åkerman <wakerman@kth.se>
2017-12-15 00:05:06 +0100

    Server check for security

--- Semi4/submitComment.php
+++ Semi4/submitComment.php
@@ -6,12 +6,13 @@ require_once './resources/fragments/init.php';
 $commentText = $_POST['commentText'];
 $recipe = $_POST['recipe'];
 use TastyRecipes\Controller\SessionManager;
-
-$controller = SessionManager::getController();
-$couldInsert = $controller->insertComment($controller->getUsername(), $commentText, $recipe);
-if ($couldInsert === TRUE) {
-    echo "Thanks for your comment, press the button below to see it below.";
-} elseif ($couldInsert === FALSE) {
-    echo "Something strange happend, try to write again";
+if ($commentText !== "") {
+    $controller = SessionManager::getController();
+    $couldInsert = $controller->insertComment($controller->getUsername(), $commentText, $recipe);
+    if ($couldInsert === TRUE) {
+        echo "Thanks for your comment, press the button below to see it below.";
+    } elseif ($couldInsert === FALSE) {
+        echo "Something strange happend, try to write again";
+    }
+    SessionManager::storeController($controller);
 }
-SessionManager::storeController($controller);
